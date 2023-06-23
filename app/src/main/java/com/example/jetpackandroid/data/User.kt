cfb0e44082daee5e147792ea01b9d4ae3d92272b
Yeonho Suh <yeonhosuh1@naver.com>
2023-06-23 15:13:15 +0900

    Room Init

---
+++ app/src/main/java/com/example/jetpackandroid/data/User.kt
@@ -0,0 +1,12 @@
+package com.example.jetpackandroid.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val Name: String,
+    val age: Int,
+)
