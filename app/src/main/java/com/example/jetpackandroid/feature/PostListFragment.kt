faf5c0c409da30449152416f7090b5106ffdb913
Yeonho Suh <yeonhosuh1@naver.com>
2023-07-01 17:31:34 +0900

    Gradle stdlib added

---
+++ app/src/main/java/com/example/jetpackandroid/feature/PostListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.jetpackandroid.feature
+
+import androidx.fragment.app.Fragment
+import com.example.jetpackandroid.R
+
+class PostListFragment : Fragment(R.layout.post_list_fragment) {
+}
